0c00fae3d33e7c7f7605cc740a45559c33aac25b
bygreencn <bygreencn@gmail.com>
2012-05-27 16:19:17 +0800

    add the batch for file rename and package

--- changeName_Hide.bat
+++ changeName_Hide.bat
@@ -1,7 +1,7 @@
 setlocal EnableDelayedExpansion 
 for /f "delims=" %%i in ('dir /a/b *.flv') do ( 
   attrib "%%i" -h
-  ren "%%i" "%%~ni.tmp"
-  attrib "%%~ni.tmp" +h
+  ren "%%i" "%%~ni.tvpm"
+  attrib "%%~ni.tvpm" +h
   )
 )
